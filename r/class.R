ce408337984fd71184f8f1d76f356c3c17d1d859
Marco Chiarandini <marco@imada.sdu.dk>
2020-03-30 15:48:39 +0200

    added class.R

--- r/class.R
+++ r/class.R
@@ -5,3 +5,30 @@ D<-read.table("../res/res.txt")
 colnames(D)<-c("alg","inst","k","cost","sec")
 boxplot(cost ~ alg, data=D, horizontal=TRUE)
 plot(D$cost,D$sec)
+
+
+library(dplyr)
+
+D<-mutate(D,alg=factor(alg))
+#D<- D %>% mutate(alg=factor(alg))
+
+D$class<-NA
+D$class[ grep("A-", D$inst)] <- "Augerat"
+D$class[ grep("CMT", D$inst)] <- "CMT"
+D$class[ grep("Golden", D$inst)] <- "Golden"
+
+D <- D %>% mutate(inst=gsub(".xml","",inst)) %>% mutate(inst=factor(inst))
+
+save(D,file="results.rda")
+load("results.rda")
+
+require(tidyr)
+require(xtable)
+xtable(spread(select(D,inst,alg,k),alg,k))
+
+require(dplyr)
+EVAL_LONG <- D %>% group_by(inst) %>% mutate(rank=rank(k,na.last = TRUE)) %>% ungroup()
+
+
+
+
